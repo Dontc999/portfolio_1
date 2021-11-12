5c5ec29ade663a8795e27628e12162c941ee0fe1
TarunChowdaryUppalapati <tarunchowdary.uppalapati@blueshieldca.com>
2021-11-12 02:24:05 -0800

    Style Guide component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'styleGuide',
+    loadChildren: () => import('./style-guide/style-guide.module').then(m => m.StyleGuideModule)
+  },
+  {
+    path: '', component: AppComponent
+  },
+  {
+    path: '', redirectTo: '/styleGuide', pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
