461cb5bfdb185ee5539d2008bf118e80eae822b7
TarunChowdaryUppalapati <tarunchowdary.uppalapati@blueshieldca.com>
2021-11-12 02:48:27 -0800

    Errors removed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,26 +12,26 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    
+
   }
-  
+
   setPagination(pageNumber: number) {
-    if (pageNumber > 2) {
-      if (pageNumber === this.displayPageSet[0]) {
-        this.displayPageSet = [pageNumber - 1, pageNumber, pageNumber + 1, pageNumber + 2];
-      } else if (pageNumber === this.displayPageSet[1]) {
-        this.displayPageSet = [pageNumber - 2, pageNumber - 1, pageNumber, pageNumber + 1];
-      } else {
-        if (pageNumber >= (this.pager.totalPages - 3)) {
-          this.displayPageSet = [this.pager.totalPages - 4, this.pager.totalPages - 3, this.pager.totalPages - 2, this.pager.totalPages - 1, this.pager.totalPages];
-        } else {
-          const removedPage = this.displayPageSet.shift();
-          this.displayPageSet.push(removedPage + 4);
-        }
-      }
-    } else {
-      this.displayPageSet = [1, 2, 3, 4];
-    }
+    // if (pageNumber > 2) {
+    //   if (pageNumber === this.displayPageSet[0]) {
+    //     this.displayPageSet = [pageNumber - 1, pageNumber, pageNumber + 1, pageNumber + 2];
+    //   } else if (pageNumber === this.displayPageSet[1]) {
+    //     this.displayPageSet = [pageNumber - 2, pageNumber - 1, pageNumber, pageNumber + 1];
+    //   } else {
+    //     if (pageNumber >= (this.pager.totalPages - 3)) {
+    //       this.displayPageSet = [this.pager.totalPages - 4, this.pager.totalPages - 3, this.pager.totalPages - 2, this.pager.totalPages - 1, this.pager.totalPages];
+    //     } else {
+    //       const removedPage = this.displayPageSet.shift();
+    //       this.displayPageSet.push(removedPage + 4);
+    //     }
+    //   }
+    // } else {
+    //   this.displayPageSet = [1, 2, 3, 4];
+    // }
   }
 
   onPagination(pageNumber: number) {
